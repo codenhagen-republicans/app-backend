a48c39a7f35621651005cd520912e8f69c15a802
Richard <rbanyi@me.com>
2018-11-24 14:09:20 +0200

    jwt boilerplate

--- app/app.py
+++ app/app.py
@@ -4,6 +4,7 @@
 from flask import Flask
 from flask_restful import Api
 import views, resources, models
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 db = models.init_db(app)
@@ -11,12 +12,21 @@ api = Api(app)
 
 
 app.config['SECRET_KEY'] = 'some-secret-string'
+app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+
+jwt = JWTManager(app)
 
 
 @app.before_first_request
 def create_tables():
     db.create_all()
 
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    jti = decrypted_token['jti']
+    return models.RevokedToken.is_jti_blacklisted(jti)
 
 api.add_resource(resources.UserRegistration, '/registration')
 api.add_resource(resources.UserLogin, '/login')
--- app/resources.py
+++ app/resources.py
@@ -1,5 +1,7 @@
+from flask_jwt_extended import (create_access_token, create_refresh_token,
+jwt_required, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt)
 from flask_restful import Resource, reqparse
-from models import User
+from models import User, RevokedToken
 
 parser = reqparse.RequestParser()
 parser.add_argument('username', help = 'This field cannot be blank', required = True)
@@ -18,8 +20,12 @@ class UserRegistration(Resource):
         )
         try:
             new_user.save_to_db()
+            access_token = create_access_token(identity = data['username'])
+            refresh_token = create_refresh_token(identity = data['username'])
             return {
-                'message': 'User {} was created'.format( data['username'])
+                'message': 'User {} was created'.format( data['username']),
+                'access_token': access_token,
+                'refresh_token': refresh_token
             }
         except:
             return {'message': 'Something went wrong'}, 500
@@ -33,24 +39,46 @@ class UserLogin(Resource):
             return {'message': 'User {} doesn\'t exist'.format(data['username'])}
 
         if User.verify_hash(data['password'], current_user.password):
-            return {'message': 'Logged in as {}'.format(current_user.username)}
+            access_token = create_access_token(identity = data['username'])
+            refresh_token = create_refresh_token(identity = data['username'])
+            return {'message': 'Logged in as {}'.format(current_user.username),
+                    'access_token': access_token,
+                    'refresh_token': refresh_token
+            }
         else:
             return {'message': 'Wrong credentials'}
 
 
 class UserLogoutAccess(Resource):
+    @jwt_required
     def post(self):
-        return {'message': 'User logout'}
+        jti = get_raw_jwt()['jti']
+        try:
+            revoked_token = RevokedToken(jti = jti)
+            revoked_token.add()
+            return {'message': 'Access token has been revoked'}
+        except:
+            return {'message': 'Something went wrong'}, 500
 
 
 class UserLogoutRefresh(Resource):
+    @jwt_refresh_token_required
     def post(self):
-        return {'message': 'User logout'}
+        jti = get_raw_jwt()['jti']
+        try:
+            revoked_token = RevokedToken(jti = jti)
+            revoked_token.add()
+            return {'message': 'Refresh token has been revoked'}
+        except:
+            return {'message': 'Something went wrong'}, 500
 
 
 class TokenRefresh(Resource):
+    @jwt_refresh_token_required
     def post(self):
-        return {'message': 'Token refresh'}
+        current_user = get_jwt_identity()
+        access_token = create_access_token(identity = current_user)
+        return {'access_token': access_token}
 
 
 class AllUsers(Resource):
@@ -62,6 +90,7 @@ class AllUsers(Resource):
 
 
 class SecretResource(Resource):
+    @jwt_required
     def get(self):
         return {
             'answer': 42
